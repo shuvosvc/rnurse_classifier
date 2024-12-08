b2f7e871e33587719810c5a536bc1ac0083028f1
sabbirlog <mridhasabbir5@gmail.com>
2024-12-09 01:27:39 +0600

    chore: finalize c  demo classifier

--- file.js
+++ file.js
@@ -3,99 +3,199 @@ const multer = require('multer');
 const sharp = require('sharp');
 const fs = require('fs').promises;
 const path = require('path');
+const Tesseract = require('tesseract.js');
 const app = express();
 
 require('dotenv').config();
 const port = process.env.PORT || 7000;
 
-const storage = multer.memoryStorage(); // Use memory storage to get the file buffer
+const storage = multer.memoryStorage();
 
-const projectUploadPath = path.join(__dirname, 'uploads'); // Assuming 'uploads' is the folder within your project
+const projectUploadPath = path.join(__dirname, 'uploads'); 
 
-// Serve static files from the 'uploads' directory
+
 app.use('/uploads', express.static(projectUploadPath));
 
 
-
-
-
 const upload = multer({
-    storage: storage,
-    fileFilter: (req, file, cb) => {
-      // Allow only JPEG, PNG, or WebP files
-      const allowedTypes = /jpeg|jpg|png|webp/;
-      const fileExt = file.originalname.split('.').pop().toLowerCase();
-      const isValidType = allowedTypes.test(fileExt);
-      if (isValidType) {
-        return cb(null, true);
-      } else {
-        return cb(new Error('Invalid file type. Only JPEG, PNG, or WebP allowed.'));
-      }
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    // Allow only JPEG, PNG, or WebP files
+    const allowedTypes = /jpeg|jpg|png|webp/;
+    const fileExt = file.originalname.split('.').pop().toLowerCase();
+    const isValidType = allowedTypes.test(fileExt);
+    if (isValidType) {
+      return cb(null, true);
+    } else {
+      return cb(new Error('Invalid file type. Only JPEG, PNG, or WebP allowed.'));
     }
-  });
+  }
+});
+
+
+
+const generateResizedImages = async (fileBuffer, originalFilename) => {
+  const currentDate = new Date().toISOString().replace(/[-:]/g, ''); // Format: YYYYMMDDTHHMMSS
+  const original = sharp(fileBuffer);
+
+
+  const processedImg = original
+    .grayscale()
+    .resize(1024, 1024, { fit: 'inside' }) 
+    .sharpen({ sigma: 1.2 }) 
+    .modulate({ contrast: 2 }) 
+    .toFormat('webp', { quality: 100 }); 
+
+  const processedImgBuffer = await processedImg.toBuffer();
+
+  const resizedImages = {
+    classifyImg: { buffer: processedImgBuffer, filename: `${originalFilename}-classifyImg-${currentDate}.webp` },
+  };
+
+  return resizedImages;
+};
+
+
+const uploadToProjectFolder = async (resizedImages) => {
+  const projectUploadPath = path.join(__dirname, 'uploads');
+  await Promise.all(
+    Object.values(resizedImages).map(async (image) => {
+      const filePath = path.join(projectUploadPath, image.filename);
+      await fs.writeFile(filePath, image.buffer);
+    })
+  );
+};
+
+
+const medicalKeywords = [
   
+  "prescription", "doctor", "physician", "surgeon", "nurse", "hospital", "clinic", "patient", "medical", "diagnosis", "treatment", 
+  "therapy", "medication", "meds", "medications", "clinical", "hospitalization", "health", "healthcare", "medical report", "treatment plan",
+  "medical history", "outpatient", "inpatient", "referral", "consultation", "consulting", "check-up", "vaccination", "screening",
+  "routine", "discharge", "medical certificate", "lab test", "test result", "test report", "health condition", "medical condition",
+
+  // Doctors and Medical Professionals
+  "dr", "dr.", "doctor", "physician", "surgeon", "cardiologist", "neurologist", "orthopedist", "dermatologist", "pediatrician", 
+  "gynecologist", "radiologist", "pathologist", "oncologist", "urologist", "dentist", "psychiatrist", "optometrist", "therapist",
+  "chiropractor", "podiatrist", "dentist", "ENT", "audiologist", "speech therapist", "clinical psychologist", "nurse practitioner",
+
+  // Diagnostic and Test-related Terms
+  "blood test", "cbc", "lipid profile", "glucose test", "blood sugar", "esr", "thyroid function test", "liver function test", "kidney function test", 
+  "urine test", "stool test", "sputum test", "biopsy", "ct scan", "mri", "ultrasound", "ecg", "x-ray", "mammogram", "pet scan", "pcr", 
+  "ultrasound scan", "endoscopy", "colonoscopy", "serum test", "hba1c", "prostate test", "glucose monitoring", "cholesterol level", 
+  "clinical examination", "blood pressure", "body temperature", "pulse rate", "oxygen saturation", "imaging", "microbiology", "cytology", 
+  "histopathology", "electrocardiogram", "electromyography", "biochemical tests",
+
+  // Medical Conditions and Diseases
+  "diabetes", "hypertension", "heart disease", "stroke", "cancer", "breast cancer", "prostate cancer", "liver cancer", "lung cancer", "colon cancer", 
+  "tuberculosis", "asthma", "arthritis", "migraine", "chronic pain", "back pain", "osteoporosis", "rheumatoid arthritis", "autoimmune disease", 
+  "alzheimers", "parkinsons", "dementia", "chronic obstructive pulmonary disease", "kidney failure", "renal disease", "hepatitis", "HIV", "AIDS", 
+  "covid-19", "covid", "flu", "influenza", "cold", "virus", "infection", "pneumonia", "sepsis", "malaria", "tuberculosis", "HIV", "syphilis",
+  "hepatitis B", "hepatitis C", "cholera", "dengue", "diabetic retinopathy", "cirrhosis", "hepatitis A", "gout", "bipolar disorder", "depression", 
+  "anxiety", "mental health", "psychiatric disorders", "schizophrenia", "bipolar", "depressive disorder", "psychosis", "post-traumatic stress disorder",
+
+  // Medications and Treatments
+  "painkiller", "analgesic", "antibiotic", "antiviral", "antifungal", "pain relief", "insulin", "glucagon", "antidepressant", "antihypertensive", 
+  "beta blocker", "antibiotics", "steroids", "chemo", "chemotherapy", "radiotherapy", "vaccination", "immunization", "antihistamine", 
+  "medication list", "pill", "tablet", "capsule", "syrup", "ointment", "cream", "inhaler", "nasal spray", "intravenous", "vaccine", 
+  "immunotherapy", "dialysis", "blood transfusion", "surgical procedure", "organ transplant", "anesthesia", "surgical removal", "anticoagulants",
+  "aspirin", "penicillin", "metformin", "ibuprofen", "paracetamol", "morphine", "antipsychotic", "anticonvulsant", "antibiotics", "statin", "insulin",
+  
+  // Common Medical Procedures
+  "surgery", "operation", "cataract surgery", "appendectomy", "heart surgery", "spinal surgery", "knee replacement", "hip replacement", 
+  "bypass surgery", "cesarean section", "plastic surgery", "cosmetic surgery", "laparoscopy", "stitch", "surgical incision", "bone marrow biopsy", 
+  "organ transplant", "dialysis", "endoscopic procedure", "bronchoscopy", "colonoscopy", "arthroscopy", "mammoplasty", "bariatric surgery",
+  
+  // Health-related Terms
+  "obesity", "weight loss", "weight management", "nutrition", "diet", "exercise", "rehabilitation", "chronic condition", "wellness", "physical therapy", 
+  "rehab", "fitness", "strengthening", "mobility", "cardiopulmonary", "diabetes management", "hypertension management", "post-surgery care", 
+  "pain management", "blood pressure control", "mental health care", "nursing care", "home care", "dietician", "nutritionist", "pediatric care", 
+  "geriatric care", "senior care", "geriatric care",
+
+  // Other Related Terms
+  "medical imaging", "laboratory", "pharmacy", "medicines", "healthcare provider", "emergency room", "urgent care", "clinical trial", 
+  "drug interaction", "medical advice", "symptom", "diagnostic test", "prescription refill", "patient care", "care plan", "medical insurance",
+  "health insurance", "health check", "preventive care", "clinical notes", "follow-up", "medical documentation", "health records", 
+  "treatment record", "patient history", "sick leave", "medical leave", "physical examination", "medical report"
+];
+
+
+
+const extractTextAndClassify = async (filePath) => {
+  try {
+    const { data: { text } } = await Tesseract.recognize(filePath, 'eng', { logger: (m) => console.log(m) });
+
+
+    const cleanedText = text.toLowerCase().replace(/\s+/g, ' ').trim();
+
+    
+    const foundKeywords = medicalKeywords.filter(keyword => cleanedText.includes(keyword));
 
   
+    const isMedicalDocument = foundKeywords.length > 0;
 
-  
+    return { extractedText: text, isMedicalDocument };
+  } catch (error) {
+    console.error("Error during OCR processing:", error);
+    throw new Error('Error processing the document.');
+  }
+};
 
-  const generateResizedImages = async (fileBuffer, originalFilename) => {
-    const currentDate = new Date().toISOString().replace(/[-:]/g, ''); // Format: YYYYMMDDTHHMMSS
-    const original = sharp(fileBuffer);
-  
-    // Convert to grayscale, resize, and apply enhancements
-    const processedImg = original
-      .grayscale()
-      .resize(1024, 1024, { fit: 'inside' }) // Resize to max 1024x1024 while preserving aspect ratio
-      .sharpen({ sigma: 1.2 }) // Enhance edges for better text clarity
-      .modulate({ contrast: 2 }) // Increase contrast
-      .toFormat('webp', { quality: 100 }); // Compress to WebP with high quality
-  
-    const processedImgBuffer = await processedImg.toBuffer();
-  
-    const resizedImages = {
-      classifyImg: { buffer: processedImgBuffer, filename: `${originalFilename}-classifyImg-${currentDate}.webp` },
+
+app.post('/api/upload', upload.single('image'), async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).send('No file uploaded.');
+    }
+
+    const originalFilename = req.file.originalname.split('.')[0];
+    const resizedImages = await generateResizedImages(req.file.buffer, originalFilename);
+
+    
+    await uploadToProjectFolder(resizedImages);
+
+   
+    const baseUrl = process.env.baseUrl || `http://localhost:${port}/uploads`;
+    const imageUrls = {
+      classifyImg: `${baseUrl}/${resizedImages.classifyImg.filename}`,
     };
-  
-    return resizedImages;
-  };
 
-  const uploadToProjectFolder = async (resizedImages) => {
-    const projectUploadPath = path.join(__dirname, 'uploads'); // Assuming 'uploads' is the folder within your project
-  
-    await Promise.all(
-      Object.values(resizedImages).map(async (image) => {
-        const filePath = path.join(projectUploadPath, image.filename);
-        await fs.writeFile(filePath, image.buffer);
-      })
-    );
-  };
-  
-  app.post('/api/upload', upload.single('image'), async (req, res) => {
-    try {
-      if (!req.file) {
-        return res.status(400).send('No file uploaded.');
-      }
-  
-      const originalFilename = req.file.originalname.split('.')[0];
-      const resizedImages = await generateResizedImages(req.file.buffer, originalFilename);
-  
-      // Upload resized images to the project folder
-      await uploadToProjectFolder(resizedImages);
-  
-      // Construct URLs based on the assumed project upload path
-      const baseUrl = process.env.baseUrl || `http://localhost:${port}/uploads`; // Replace with your actual domain
-      const imageUrls = {
-        classifyImg: `${baseUrl}/${resizedImages.classifyImg.filename}`,
-      };
-  
-      res.json(imageUrls);
-    } catch (error) {
-      console.error('Error processing/uploading image:', error);
-      res.status(500).send('Internal Server Error');
-    }
-  });
-  
+   
+    const result = await extractTextAndClassify(`${projectUploadPath}/${resizedImages.classifyImg.filename}`);
+
+
+    res.json({
+      imageUrl: imageUrls.classifyImg,
+      extractedText: result.extractedText,
+      isMedicalDocument: result.isMedicalDocument ? 'Yes' : 'No',
+    });
+  } catch (error) {
+    console.error('Error processing/uploading image:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 const deleteImagesByPaths = async (imagePaths) => {
